a01da2fc935263fd6b9ac6c60616023cd5c0280e
MartiMarch <mmll1199@protonmail.com>
2024-10-21 23:19:29 +0200

    feat: gitea preparado

--- chart/install.sh
+++ chart/install.sh
@@ -1,7 +1,12 @@
 helm repo add jenkins https://charts.jenkins.io
 # helm search repo jenkins --versions
-helm repo add sonatype https://sonatype.github.io/helm3-charts/
-# helm search repo sonatype/nexus-repository-manager --versions
+
+# Remokazado por Docker Hub
+#helm repo add sonatype https://sonatype.github.io/helm3-charts/
+#helm search repo sonatype --versions
+
+helm repo add gitea-charts https://dl.gitea.com/charts/
+#helm search repo gitea --versions
 helm repo update
 rm -rf Chart.lock
 helm dependency build
