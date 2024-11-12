1e37a8d3f4774d924e2fbe9a2bb3343b4162661c
MartiMarch <mmll1199@protonmail.com>
2024-11-12 19:51:27 +0100

    feat: .

--- chart/install.sh
+++ chart/install.sh
@@ -1,14 +1,18 @@
+## JENKINS
 helm repo add jenkins https://charts.jenkins.io
 # helm search repo jenkins --versions
 
-# Remokazado por Docker Hub
-#helm repo add sonatype https://sonatype.github.io/helm3-charts/
+## NEXUS SONATYPE
+helm repo add sonatype https://sonatype.github.io/helm3-charts/
 #helm search repo sonatype --versions
 
+## GITEA
 helm repo add gitea-charts https://dl.gitea.com/charts/
 #helm search repo gitea --versions
+
 helm repo update
 rm -rf Chart.lock
+helm dependency build
 helm dependency build
 helm -n autojenkins delete autojenkins
 sleep 30
